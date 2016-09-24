daf974df583d48cacf60008ed4cce6b9e2c00dfb
snagasawa <whitecrane8920@gmail.com>
2016-09-24 22:09:12 +0900

    Move the rc files from dropbox to google drive

--- .aliases.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ln -sf ~/dropbox/dotfiles/home/.vimrc ~/.vimrc
-ln -sf ~/dropbox/dotfiles/home/.zshrc ~/.zshrc
-
-source ~/.zshrc
-source ~/.vimrc
---
+++ aliases.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln ~/google_drive/dotfiles/home/vimrc ~/.vimrc
+ln ~/google_drive/dotfiles/home/zshrc ~/.zshrc
+
+source ~/.zshrc
+source ~/.vimrc
